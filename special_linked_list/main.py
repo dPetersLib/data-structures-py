45ab812dd9bd5f52abec577a45ba3137ae3ee7b5
peter4ease <40390930+peter4ease@users.noreply.github.com>
2022-09-19 14:25:20 +0100

    basic functions ok

--- special_linked_list/main.py
+++ special_linked_list/main.py
@@ -7,7 +7,7 @@ def special_list(values, print_backwards):
     return d.print_backwards() if print_backwards else d.print_forward()
     ### DO NOT MODIFY ABOVE
 
-    
+
 class Node():
     def __init__(self, data, prev, next):
         self.data = data
@@ -19,10 +19,37 @@ class MySpecialList():
     my_list = []
 
     def append(self, data):
-        return
+        list_lenght = len(self.my_list)
+        
+        next = None
+        
+        if list_lenght == 0:
+            prev = None
+
+        elif list_lenght > 0:
+            last_item = self.my_list[-1]
+            prev = last_item
+            last_item.next = Node(data, prev, next)
+            
+        self.my_list.append(Node(data, prev, next))
+        return self.my_list
     
     def print_forward(self):
-        return
+        list = []
+        for item in self.my_list:
+            list.append(item.data)
+        return list
     
     def print_backwards(self):
-        return
+        reverse_list = []
+        list_lenght = len(self.my_list)
+        i = list_lenght - 1
+
+        while i >= 0:
+            reverse_list.append(self.my_list[i].data)
+            i  -= 1
+
+        return reverse_list
+
+# print(special_list([1,2,3,4,5], False))
+print(special_list([1,2,3,4,5], True))
